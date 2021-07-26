5c5af03c9939bc1d731401ba03ac0d36e085b58f
Ivanov Pavel <apostol_fm@mail.ru>
2021-07-26 11:25:28 +0200

    button

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Button, SafeAreaView} from 'react-native';
+
+
+const apiKey = 'ea505b5c-e873-4b18-86ca-d855815a2cbc'
+const url = 'https://api.thecatapi.com/v1/images/search'
 
 export default function App() {
+
+  const getRandomCat = () =>{ 
+   // console.log('catURL')
+fetch(url)
+    .then((res)=>res.json())
+    .then((cats)=>{console.log('cats',cats)})
+    .catch((error)=>{console.log('Error',error)})
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Здесь будут КОТИКИ!!!</Text>
+    <SafeAreaView style={styles.container}>
+
+      <Button title={'дайте-ка котика!'} onPress={getRandomCat} />
+      <Text>Здесь будут  КОТИКИ!!!</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#a4d8fc',
     alignItems: 'center',
     justifyContent: 'center',
   },
