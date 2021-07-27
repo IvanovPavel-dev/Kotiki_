9ca0ff0a1d494def9743b7dd6d1275d9be1930b5
Ivanov Pavel <apostol_fm@mail.ru>
2021-07-27 10:03:24 +0200

    fetch, getACat, carUrl

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View ,Button, SafeAreaView} from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View ,Button, SafeAreaView, Image} from 'react-native';
+
 
 
 const apiKey = 'ea505b5c-e873-4b18-86ca-d855815a2cbc'
@@ -8,23 +9,37 @@ const url = 'https://api.thecatapi.com/v1/images/search'
 
 export default function App() {
 
+const [catUrl,setCatUrl]= useState('')
+
+
   const getRandomCat = () =>{ 
-   // console.log('catURL')
+  
 fetch(url)
     .then((res)=>res.json())
-    .then((cats)=>{console.log('cats',cats)})
+    .then((cats)=>{
+      console.log('cats',cats)
+      const catUrl = cats[0].url
+      setCatUrl(catUrl)
+    })
     .catch((error)=>{console.log('Error',error)})
   }
-
+console.log(catUrl)
   return (
     <SafeAreaView style={styles.container}>
-
+      
       <Button title={'дайте-ка котика!'} onPress={getRandomCat} />
       <Text>Здесь будут  КОТИКИ!!!</Text>
+      
+      
       <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
+
+// <Image source={{
+//         width : 300,
+//         uri : 'https://pikabu.ru/story/kotov_ne_vyibirayut_8244093'}} />
+
 
 const styles = StyleSheet.create({
   container: {
