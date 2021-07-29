b1a2a70bd5755d811cf9e05e1562a277d1cd96d0
Ivanov Pavel <apostol_fm@mail.ru>
2021-07-29 13:16:21 +0200

    Main&Favorites components

--- App.js
+++ App.js
@@ -1,37 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { StyleSheet, Text, View ,Button, SafeAreaView, Image } from 'react-native';
-
-const apiKey = 'ea505b5c-e873-4b18-86ca-d855815a2cbc'
-const url = 'https://api.thecatapi.com/v1/images/search'
+//import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, } from 'react-native';
+import Main from './components/Main'
+import Favorites from './components/Favorites'
 
 export default function App() {
 
-const [catUrl,setCatUrl]= useState('https://cs.pikabu.ru/images/big_size_comm/2012-11_4/13533113323571.jpg')
-
-  const getRandomCat = () =>{ 
-  
-fetch(url)
-    .then((res)=>res.json())
-    .then((cats)=>{
-      
-      const catUrl = cats[0].url
-      setCatUrl(catUrl)
-    })
-    .catch((error)=>{console.log('Error',error)})
-  }
-console.log(catUrl)
   return (
-    <View style={styles.container}>
-      <Image
-        style={styles.logo}
-        source={{
-          uri: catUrl,
-        }}
-      />
-      <Button title={'дайте-ка котика!'} onPress={getRandomCat} />
-      <StatusBar style="auto" />
-    </View>
+    <Main />
   );
 }
 
@@ -46,5 +22,6 @@ const styles = StyleSheet.create({
   logo: {
     width: 250,
     height: 250,
+    borderRadius: 50
   },
 });
